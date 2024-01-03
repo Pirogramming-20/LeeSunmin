0862fcde1b55184b859caabc5b4b1bbae94c43dc
Phoebe125 <ted9988sm9988@gmail.com>
2024-01-03 16:26:18 +0900

    숫자야구 input reset 및 최종 버전

--- 숫자야구 게임/script.js
+++ 숫자야구 게임/script.js
@@ -1,18 +1,18 @@
 // 초기 세팅
 // 1. play 하는 게임 라운드 설정
-let gameRound = 9; 
+let gameRound = 9;
 
 // 2. 랜덤하게 숫자 3개 뽑기
-let randomNumberArray = []
-let number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-for (let i=0; i<3; i++) {
-    const index = Math.floor(Math.random() * (number.length-i));
-    randomNumberArray.push(number[index]);
-    number.splice(index, 1);
+let randomNumberArray = [];
+let number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+for (let i = 0; i < 3; i++) {
+  const index = Math.floor(Math.random() * (number.length - i));
+  randomNumberArray.push(number[index]);
+  number.splice(index, 1);
 }
 console.log("answer: " + randomNumberArray);
 
-// 결과 표시 창 초기화
+// 3. 결과 표시 창 초기화
 function reset_result() {
   const resultBox = document.querySelector(".result-display");
   while (resultBox.firstChild) {
@@ -22,22 +22,24 @@ function reset_result() {
 reset_result();
 
 // 결과 출력 함수
-function add_result (inputList, play_output) {
-    const flag = function (play_output) {
-      return play_output.every(function (element) { return element == 0; });
-    };
-    if (flag(play_output)) {
-      yes_out_result(inputList, play_output);
-    } else {
-      no_out_result(inputList, play_output);
-    }
-    const resultContainer = document.querySelector(".result-display");
-    resultContainer.scrollTop = resultContainer.scrollHeight;
+function add_result(inputList, play_output) {
+  const flag = function (play_output) {
+    return play_output.every(function (element) {
+      return element == 0;
+    });
+  };
+  if (flag(play_output)) {
+    yes_out_result(inputList, play_output);
+  } else {
+    no_out_result(inputList, play_output);
+  }
+  const resultContainer = document.querySelector(".result-display");
+  resultContainer.scrollTop = resultContainer.scrollHeight;
 }
 
-// 결과 출력 함수
+// 결과 출력 함수 1: out 이 없을 때 출력
 function no_out_result(inputList, play_output) {
-  const parentElement = document.querySelector(".result-display"); 
+  const parentElement = document.querySelector(".result-display");
 
   const newResultDiv = document.createElement("div");
   newResultDiv.className = "check-result";
@@ -72,6 +74,7 @@ function no_out_result(inputList, play_output) {
   parentElement.appendChild(newResultDiv);
 }
 
+// 결과 출력 함수 2: out 이 있을 때 출력
 function yes_out_result(inputList, play_output) {
   const parentElement = document.querySelector(".result-display");
 
@@ -99,66 +102,46 @@ function yes_out_result(inputList, play_output) {
   parentElement.appendChild(newResultDiv);
 }
 
+// 입력값 확인 함수
+// 1. 입력값 초기화 함수
+function reset_input() {
+  const inputObj = document.querySelector(".input-box").children;
+  for (let i = 0; i < 3; i++) {
+    inputObj[i].value = "";
+  }
+}
 
-// check_numbers 함수에서 사용할 함수
-function check_inputs(){
+//2. 입력값 확인 + 유효하면 입력값 list로 반환
+function check_inputs() {
   const inputObj = document.querySelector(".input-box").children;
   const inputNumbers = [];
   for (let i = 0; i < 3; i++) {
     inputNumbers.push(inputObj[i].value);
   }
-  // 1. 만일 숫자가 3개 모두 입력되지 않은 경우
-  if (inputNumbers.includes('')){
-    // input box 초기화
-    for (let i = 0; i < 3; i++) {
-      inputObj[i].value = '';
-    }
+  if (inputNumbers.includes("")) {
+    reset_input();
     return false;
-  }
-  // 2. 만일 숫자가 3개 모두 입력된 경우 -> 리스트 출력
-  else {
+  } else {
+    reset_input();
     return inputNumbers;
   }
 }
 
+// 게임 진행 함수: [strike, ball] 반환
 function play_game(inputList) {
-    let strike = 0;
-    let ball = 0;
-    // strike 와 ball 개수 세기
-    for (let i=0; i<3; i++) {
-        if (inputList[i] == randomNumberArray[i]) {
-            strike++;
-        }
-        else if (randomNumberArray.includes(Number(inputList[i]))){
-          ball++;
-        }
+  let strike = 0;
+  let ball = 0;
+  for (let i = 0; i < 3; i++) {
+    if (inputList[i] == randomNumberArray[i]) {
+      strike++;
+    } else if (randomNumberArray.includes(Number(inputList[i]))) {
+      ball++;
     }
-    return [strike, ball];
+  }
+  return [strike, ball];
 }
 
-// submit 버튼 누르면 실행되는 함수
-function check_numbers() {
-    console.log("left game round: " + gameRound);
-    let success_flag;
-    let play_output;
-    let inputList = check_inputs(); 
-    if (inputList !== false) {
-      gameRound--;
-      play_output=play_game(inputList);
-      add_result(inputList, play_output);
-    }
-
-    // 성공 or 실패 여부 따라 이미지 띄우기
-    if (gameRound > 0 & play_output[0]==3) {
-      success_flag = true;
-      display_result(success_flag);
-    }
-    else if (gameRound <= 0) {
-      success_flag = false;
-      display_result(success_flag);
-    }
-}
-
+// 결과 image 출력 함수
 function display_result(success_flag) {
   const resultDiv = document.querySelector(".game-result");
   const resultImg = document.createElement("img");
@@ -169,7 +152,27 @@ function display_result(success_flag) {
   }
   resultDiv.appendChild(resultImg);
 
-  // 버튼 비활성화
-  const myButton = document.querySelector(".submit-button"); 
+  const myButton = document.querySelector(".submit-button");
   myButton.disabled = true;
+}
+
+// submit 버튼 누르면 실행되는 함수
+function check_numbers() {
+  let success_flag;
+  let play_output;
+  let inputList = check_inputs();
+  if (inputList !== false) {
+    gameRound--;
+    play_output = play_game(inputList);
+    add_result(inputList, play_output);
+    // 성공 or 실패 여부 따라 이미지 띄우기
+    if ((gameRound > 0) & (play_output[0] == 3)) {
+      success_flag = true;
+      display_result(success_flag);
+    } else if (gameRound <= 0) {
+      success_flag = false;
+      display_result(success_flag);
+    }
+  }
+  console.log("left game round: " + gameRound);
 }
