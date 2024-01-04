9913739adb16de02047c94d069121d126c7a7c1c
Phoebe125 <ted9988sm9988@gmail.com>
2024-01-04 18:49:54 +0900

    3단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,3 +1,23 @@
-num=0
+num = 0
 
-input_num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+
+def validate_input():
+    flag = True
+    while flag:
+        input_num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+
+        # 정수 확인
+        if not input_num.isdigit():
+            print("정수를 입력하세요")
+            continue
+
+        # 범위 확인
+        if int(input_num) not in [1, 2, 3]:
+            print("1,2,3 중 하나를 입력하세요")
+        else:  # 최종 okay
+            flag = False
+    return int(input_num)
+
+
+if __name__ == "__main__":
+    input_num = validate_input()
