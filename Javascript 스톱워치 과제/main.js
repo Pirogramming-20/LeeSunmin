a48c958b94604561ee78405517e5851a4bc3fb3a
Phoebe125 <ted9988sm9988@gmail.com>
2024-01-05 21:20:36 +0900

    전체 삭제 및 전체 선택 버튼까지 구현 완료

--- Javascript 스톱워치 과제/main.js
+++ Javascript 스톱워치 과제/main.js
@@ -6,33 +6,39 @@ const second = document.getElementById("sec");
 const milli_second = document.getElementById("milli-sec");
 const recordList = document.getElementById("records-list");
 const recordLiElements = recordList.querySelectorAll("li");
+const allDeleteBtn = document.getElementById("all-delete-button");
+
+const binBtn = document.getElementById("bin-button");
 
 let seconds = 0;
 let tenMill = 0;
 
 startBtn.addEventListener("click", () => {
-    console.log("startBtn action");
-    intervalId = setInterval(operateTimer, 10);
+  console.log("startBtn action");
+  intervalId = setInterval(operateTimer, 10);
 });
 
 stopBtn.addEventListener("click", () => {
-    console.log("stopBtn action");
-    clearInterval(intervalId);
-    add_record();
+  console.log("stopBtn action");
+  clearInterval(intervalId);
+  add_record();
 });
 
 resetBtn.addEventListener("click", () => {
-    console.log("resetBtn action");
-    seconds = 0;
-    tenMill = 0;
-    second.innerHTML = "00";
-    milli_second.innerHTML = "00";
+  console.log("resetBtn action");
+  seconds = 0;
+  tenMill = 0;
+  second.innerHTML = "00";
+  milli_second.innerHTML = "00";
 });
+
+binBtn.addEventListener("click", delete_record);
 
 recordLiElements.forEach((liElement) => {
-    liElement.addEventListener("click", add_check);
+  liElement.addEventListener("click", add_check);
 });
 
+allDeleteBtn.addEventListener("click", delete_all_records);
 
 function operateTimer() {
   tenMill++;
@@ -61,11 +67,100 @@ function add_record() {
   recordList.appendChild(li);
   // 새로 생성된 li 태그에 이벤트 리스너 추가
   li.addEventListener("click", add_check);
+  allDeleteBtn.addEventListener("click", delete_all_records);
 }
 
 function add_check(e) {
   console.log("liElement action");
-  e.target.classList.remove("ri-circle-line");
-  e.target.classList.add("ri-checkbox-circle-line");
+  console.log(e.target.tagName);
+  if (e.target.tagName === "I") {
+    if (e.target.classList.contains("ri-checkbox-circle-line")) {
+      e.target.classList.remove("ri-checkbox-circle-line");
+      e.target.classList.add("ri-circle-line");
+
+      // 전체 노드 체크 해제되도록
+      if (allDeleteBtn.classList.contains("ri-checkbox-circle-line")) {
+        allDeleteBtn.classList.remove("ri-checkbox-circle-line");
+        allDeleteBtn.classList.add("ri-circle-line");
+      }
+
+      return;
+    } else {
+      e.target.classList.remove("ri-circle-line");
+      e.target.classList.add("ri-checkbox-circle-line");
+
+      //전체 노드 체크 되었는지 확인하고, 전체 체크되어있으면 전체 삭제 체크
+      let flag = true;
+      const currentLiElements = Array.from(recordList.querySelectorAll("li"));
+      currentLiElements.forEach((liElement) => {
+        if (liElement.children[0].classList.contains("ri-circle-line")) {
+          flag = false;
+          return;
+        }
+      });
+
+      if (flag === true && allDeleteBtn.classList.contains("ri-circle-line")) {
+        allDeleteBtn.classList.remove("ri-circle-line");
+        allDeleteBtn.classList.add("ri-checkbox-circle-line");
+      }
+    }
+  }
 }
 
+// 체크된 항목 삭제
+function delete_record() {
+  const checkedLiElements = recordList.querySelectorAll(
+    ".ri-checkbox-circle-line"
+  );
+  const parentElements = Array.from(checkedLiElements).map(
+    (element) => element.parentNode
+  );
+  console.log(parentElements);
+  parentElements.forEach((liElement) => {
+    console.log(liElement);
+    liElement.remove();
+  });
+  if (allDeleteBtn.classList.contains("ri-checkbox-circle-line")) {
+    allDeleteBtn.classList.remove("ri-checkbox-circle-line");
+    allDeleteBtn.classList.add("ri-circle-line");
+  }
+  console.log("delete_record action");
+  console.log(checkedLiElements);
+}
+
+// 전체 선택 및 해제
+function delete_all_records(e) {
+  console.log(e.target.tagName);
+  if (e.target.tagName === "I") {
+    const currentLiElements = Array.from(recordList.querySelectorAll("li"));
+    // 체크가 안되어 있으면 체크되도록
+    if (e.target.classList.contains("ri-circle-line")) {
+      e.target.classList.remove("ri-circle-line");
+      e.target.classList.add("ri-checkbox-circle-line");
+
+      // 자식 노드들도
+      currentLiElements.forEach((liElement) => {
+        if (liElement.children[0].classList.contains("ri-circle-line")) {
+          liElement.children[0].classList.remove("ri-circle-line");
+          liElement.children[0].classList.add("ri-checkbox-circle-line");
+          return;
+        }
+      });
+    } else {
+      //체크가 되어있으면 체크해제하도록
+      e.target.classList.remove("ri-checkbox-circle-line");
+      e.target.classList.add("ri-circle-line");
+
+      //자식 노드들도
+      currentLiElements.forEach((liElement) => {
+        if (
+          liElement.children[0].classList.contains("ri-checkbox-circle-line")
+        ) {
+          liElement.children[0].classList.remove("ri-checkbox-circle-line");
+          liElement.children[0].classList.add("ri-circle-line");
+          return;
+        }
+      });
+    }
+  }
+}
