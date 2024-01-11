c5fde7d707763c061922227ee3ad56414de648b6
Phoebe125 <ted9988sm9988@gmail.com>
2024-01-11 18:31:02 +0900

    [feat 3] 새 리뷰 작성 초안

--- myMovieReviews/review/urls.py
+++ myMovieReviews/review/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path("", reviews_list),
     path("<int:pk>", review_detail),
+    path("create", review_create),
 ]
--- myMovieReviews/review/views.py
+++ myMovieReviews/review/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 
 # Create your views here.
+
+
 def reviews_list(request):
     reviews = Review.objects.all()
     context = {
@@ -9,9 +11,26 @@ def reviews_list(request):
     }
     return render(request, "review/reviews_list.html", context)
 
+
 def review_detail(request, pk):
     review = Review.objects.get(id=pk)
     context = {
         "review": review
     }
     return render(request, "review/review_detail.html", context)
+
+
+def review_create(request):
+    if request.method == "POST":
+        Review.objects.create(
+            title=request.POST["title"],
+            release_year=request.POST["release_year"],
+            genre=request.POST["genre"],
+            score=request.POST["score"],
+            director=request.POST["director"],
+            main_actor=request.POST["main_actor"],
+            running_time=request.POST["running_time"],
+            content=request.POST["content"],
+        )
+        return redirect("/review")
+    return render(request, "review/review_create.html")
